8d06b4f016d4774bf85cf50c7d5555cbd083163c
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2016-11-01 23:47:49 +0530

    not working costas loop, almost there

--- python/costas_loop.py
+++ python/costas_loop.py
@@ -20,60 +20,80 @@
 # -
 
 import numpy as np
+import math
 from gnuradio import gr
 import costas8
 from gnuradio import blocks
 from gnuradio import filter
 
 class costas_loop(gr.sync_block):
-    """
-    docstring for block costas_loop
-    """
-    def __init__(self, samp_rate, iter):
-        gr.sync_block.__init__(self,
-            name="costas_loop",
-            in_sig=[np.complex64],
-            out_sig=[np.complex64])
-        self.samp_rate = samp_rate;
-        self.iter = iter;
+  """
+  docstring for block costas_loop
+  """
+  def __init__(self, samp_rate, iter):
+    gr.sync_block.__init__(self,
+      name="costas_loop",
+      in_sig=[np.complex64],
+      out_sig=[np.complex64])
+    self.samp_rate = samp_rate;
+    self.iter = iter;
 
-    ##################################################
-        # Blocks
-    ##################################################
-        
-        self.iir_filter_ffd_0 = filter.iir_filter_ffd(([1.0001,-1]), ([-1,1]), True)
-        self.costas8_sp_threshold_1 = costas8.sp_threshold()
-        self.costas8_sp_threshold_0 = costas8.sp_threshold()
-        self.blocks_vco_c_0 = blocks.vco_c(samp_rate, -5, 1)
-        self.set_history(2);         # For the filter block
+  ##################################################
+    # Blocks
+  ##################################################
 
+    self.prev_output_iir = 0
+    self.costas8_sp_threshold_1 = costas8.sp_threshold()
+    self.costas8_sp_threshold_0 = costas8.sp_threshold()
+    self.k_factor = -5.0/samp_rate
+    self.set_history(2);     # For the filter block
 
-    def work(self, input_items, output_items):
-        in0 = input_items[0]
-        out = output_items[0]
-        # <+signal processing here+>
-        loop_input_prev = np.ones(in0.shape)
-        loop_input_curr = in0
-        op_thresh_imag = np.ones(in0.shape)
-        op_thresh_real = np.ones(in0.shape)
-        out_iir = np.ones(in0.shape)
-        out_vco = np.ones(in0.shape)
-        for i in xrange(1,self.iter):
-            on_first_mul = loop_input_curr*loop_input_prev
-            real = on_first_mul.real
-            imag = on_first_mul.imag
-            import pdb
-            pdb.set_trace()
-            np.array([op_thresh_imag])[0][np.array([imag])[0] > -1] = 1
-            self.costas8_sp_threshold_0.work(np.array([imag]), np.array([op_thresh_imag]))
-            self.costas8_sp_threshold_1.work(real, op_thresh_real)
-            in_iir = imag*op_thresh_real - real*op_thresh_imag
-            self.iir_filter_ffd_0.work(in_iir, out_iir)
-            self.blocks_vco_c_0.work(out_iir, out_vco)
-            loop_input_prev = loop_input_curr
-            loop_input_curr = out_vco
+  def sp_threshold(self, in0):
+    output = 0
+    if in0 <= math.cos(7*math.pi/8):
+      output = -1
+    elif in0 > math.cos(7*math.pi/8) and in0 <= math.cos(5*math.pi/8):
+      output = -1/(2**0.5)
+    elif in0 > math.cos(5*math.pi/8) and in0 <= math.cos(3*math.pi/8):
+      output = 0
+    elif in0 > math.cos(3*math.pi/8) and in0 <= math.cos(math.pi/8):
+      output = 1/(2**0.5)
+    elif in0 > math.cos(math.pi/8):
+      output = 1
+    return output
 
-        out[:] = loop_input[self.iter]*loop_input[self.iter-1]
-        
-        return len(output_items[0])
+  def work(self, input_items, output_items):
+    in0 = input_items[0]
+    out = output_items[0]
+    # <+signal processing here+>
+    feedback = 1
+    on_first_mul = np.ones(in0.shape, dtype=np.complex64)
+    op_thresh_imag = np.ones(in0.shape, dtype=np.complex64)
+    op_thresh_real = np.ones(in0.shape, dtype=np.complex64)
+    in_iir = np.zeros(in0.shape, dtype=np.complex64)
+    out_iir = np.ones(in0.shape, dtype=np.complex64)
+    out_vco = np.ones(in0.shape, dtype=np.complex64)
+    prev_output_iir = np.zeros(in0.shape, dtype=np.complex64)
+    prev_output_iir[0] = self.prev_output_iir
+    for i in xrange(1,self.iter):
+      on_first_mul = in0*feedback
+      real = on_first_mul.real
+      imag = on_first_mul.imag
+      #op_thresh_imag = self.sp_threshold(imag)
+      #op_thresh_real = self.sp_threshold(real)
+      self.costas8_sp_threshold_0.work(np.array([imag]), np.array([op_thresh_imag]))
+      self.costas8_sp_threshold_1.work(np.array([real]), np.array([op_thresh_real]))
+      in_iir = imag*op_thresh_real - real*op_thresh_imag
+      in_iir2 = np.concatenate([[0],in_iir[0:-1]])
+      out_iir = in_iir*1.001 - in_iir2 + prev_output_iir
+      real_part = np.cos(self.k_factor*out_iir)
+      imag_part = np.sin(self.k_factor*out_iir)
+      out_vco = real_part + 1j*imag_part
+      feedback = out_vco
+      prev_output_iir = np.concatenate([[0],out_iir[0:-1]])
+      if i == 1:
+        self.prev_output_iir = out_iir[-1]
+    out[:] = (on_first_mul)[1:]
+
+    return len(output_items[0])
 
